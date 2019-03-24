976c5a42f5bdee70d00cfbef68b3f501ae23dd1d
hybridbad <hybridbad90@gmail.com>
2019-03-24 19:21:32 +0000

    test drove player class, added global vars

--- spec/attack_spec.rb
+++ spec/attack_spec.rb
@@ -1,7 +1,7 @@
 feature 'Attacking' do
   scenario 'attack Player 2' do
     sign_in_and_play
-    click_link 'Attack'
-    expect(page).to have_content 'Dave attacked Mittens'
+    click_button 'Attack'
+    expect(page).to have_content 'James attacked Emily'
   end
 end
